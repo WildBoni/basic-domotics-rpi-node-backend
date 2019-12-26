7309edd05516f6d1dc95dd1c5336b6beb2d780f0
WildBoni <wild_boni@hotmail.it>
2019-12-26 20:13:14 +0000

    bluetooth connection ok

--- backend/controllers/routes.js
+++ backend/controllers/routes.js
@@ -1,30 +1,25 @@
 const bluetooth = require('node-bluetooth');
-
+const device = new bluetooth.DeviceINQ();
 exports.getData = (req, res, next) => {
   device
     .on('finished', console.log.bind(console, 'finished'))
     .on('found', function found(address, name) {
       console.log('found: ' + address + ' with name ' + name);
-
+if(name == "HC-06") {
+	
       device.findSerialPortChannel(address, function(channel) {
         console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
 
         bluetooth.connect(address, channel, function(err, connection) {
           if(err) return console.log(err);
-          connection.write(new Buffer('Hello!', 'utf-8'));
+
+          connection.write(Buffer.from('H', 'utf-8'), (err) => {
+		  if(err) console.log(err);
+	 });
         });
       });
-
-      bluetooth.connect(address, channel, function(err, connection) {
-        if(err) return console.log(err);
-
-        connection.on('data', (buffer) => {
-          console.log('received message', buffer.toString());
-        });
-
-        connection.write(new Buffer('Hello!', 'utf-8'));
-      });
-    }).inquire();
+}	
+    }).scan();
 
   res.status(200).json({
     message: 'Here\'s the response!'
