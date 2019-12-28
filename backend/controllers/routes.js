c390da0070fa452a0f8cc9f35be31342ffe7a395
WildBoni <wild_boni@hotmail.it>
2019-12-28 21:20:06 +0000

    optimizing backend and frontend

--- backend/controllers/routes.js
+++ backend/controllers/routes.js
@@ -1,22 +1,29 @@
 const bluetooth = require('node-bluetooth');
 const device = new bluetooth.DeviceINQ();
+
+var indirizzo, canale, connessione;
+
+device
+.on('finished', console.log.bind(console, 'finished'))
+.on('found', function found(address, name) {
+  console.log('found: ' + address + ' with name ' + name);
+  if(name == "HC-06") {
+    device.findSerialPortChannel(address, function(channel) {
+      console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+      indirizzo = address;
+      canale = channel;
+      bluetooth.connect(address, channel, function(err, connection) {
+        if(err) return console.log(err);
+        connessione = connection;
+      })
+    });
+  }
+}).scan();
+
 exports.lightOn = (req, res, next) => {
-  device
-    .on('finished', console.log.bind(console, 'finished'))
-    .on('found', function found(address, name) {
-      console.log('found: ' + address + ' with name ' + name);
-      if(name == "HC-06") {
-        device.findSerialPortChannel(address, function(channel) {
-          console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
-          bluetooth.connect(address, channel, function(err, connection) {
-            if(err) return console.log(err);
-            connection.write(Buffer.from('H', 'utf-8'), (err) => {
-              if(err) console.log(err);
-            });
-          });
-        });
-      }
-  }).scan();
+  connessione.write(Buffer.from('H', 'utf-8'), (err) => {
+    if(err) console.log(err);
+  });
 
   res.status(200).json({
     message: 'Here\'s the response!'
@@ -24,22 +31,9 @@ exports.lightOn = (req, res, next) => {
 }
 
 exports.lightOff = (req, res, next) => {
-  device
-    .on('finished', console.log.bind(console, 'finished'))
-    .on('found', function found(address, name) {
-      console.log('found: ' + address + ' with name ' + name);
-      if(name == "HC-06") {
-        device.findSerialPortChannel(address, function(channel) {
-          console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
-          bluetooth.connect(address, channel, function(err, connection) {
-            if(err) return console.log(err);
-            connection.write(Buffer.from('L', 'utf-8'), (err) => {
-              if(err) console.log(err);
-            });
-          });
-        });
-      }
-  }).scan();
+  connessione.write(Buffer.from('L', 'utf-8'), (err) => {
+    if(err) console.log(err);
+  });
 
   res.status(200).json({
     message: 'Here\'s the response!'
