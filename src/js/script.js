b1a89b428505328def2986a7d67259ecd11a5441
WildBoni <web@davidebonifacio.it>
2020-04-01 18:07:18 +0100

    minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkbox = document.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener('change', function() {
-      if(checkbox.checked) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            var resp = JSON.parse(xhttp.responseText);
-            if(resp.response == "H") {
-                document.getElementById("status").innerHTML = "Switching ON";
-            }
+  var checkbox = document.querySelector('input[type="checkbox"]');
+  checkbox.addEventListener('change', function() {
+    document.getElementById("status").innerHTML = "Waiting for response...";
+    if(checkbox.checked) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var resp = JSON.parse(xhttp.responseText);
+          if(resp.response == "H") {
+              document.getElementById("status").innerHTML = "Switched ON";
           }
-        };
-        xhttp.open("GET", "data/on", true);
-        xhttp.send();
-      } else {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            var resp = JSON.parse(xhttp.responseText);
-            if(resp.response == "L") {
-                document.getElementById("status").innerHTML = "Switching OFF";
-            }
+        }
+      };
+      xhttp.open("GET", "data/on", true);
+      xhttp.send();
+    } else {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var resp = JSON.parse(xhttp.responseText);
+          if(resp.response == "L") {
+              document.getElementById("status").innerHTML = "Switched OFF";
           }
-        };
-        xhttp.open("GET", "data/off", true);
-        xhttp.send();
-      }
-    })
+        }
+      };
+      xhttp.open("GET", "data/off", true);
+      xhttp.send();
+    }
   })
+})
